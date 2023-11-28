aaa1fc942a532371e37abdcec538d01c3f4b00fb
001hari <pradhanharikrushna05@gmail.com>
2023-11-28 22:10:58 +0530

    Uploding to Github

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const xhr = new XMLHttpRequest();
+  const url = 'https://api.quotable.io/random';
+  const pera = document.getElementById('pera');
+
+  function handleResponse() {
+      if (xhr.readyState === 4) {
+          if (xhr.status >= 200 && xhr.status < 300) {
+              var data = JSON.parse(xhr.responseText);
+              const quote = data.content;
+              processContent(quote);
+          } else {
+              console.error('Error fetching quote:', xhr.statusText);
+          }
+      }
+  }
+
+  function processContent(quote) {
+      pera.innerHTML = quote;
+  }
+
+  xhr.onreadystatechange = handleResponse;
+  xhr.open('GET', url);
+  xhr.send();
+});
